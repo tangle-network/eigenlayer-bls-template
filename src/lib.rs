df26912610de3a70da144cd043862e6b93e63275
Tjemmmic <mdtjemmes@proton.me>
2025-01-22 14:31:18 -0600

    fix: use blueprint sdk

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
-use crate::sdk::load_abi;
-use alloy_sol_types::sol;
-use gadget_sdk as sdk;
+use blueprint_sdk::alloy::primitives::{address, Address};
+use blueprint_sdk::alloy::rpc::types::Log;
+use blueprint_sdk::alloy::sol;
+use blueprint_sdk::config::GadgetConfiguration;
+use blueprint_sdk::event_listeners::evm::EvmContractEventListener;
+use blueprint_sdk::job;
+use blueprint_sdk::macros::load_abi;
+use blueprint_sdk::std::convert::Infallible;
+use blueprint_sdk::std::sync::LazyLock;
 use serde::{Deserialize, Serialize};
 
-use alloy_primitives::{address, Address};
-use gadget_sdk::event_listener::evm::contracts::EvmContractEventListener;
-use std::{convert::Infallible, ops::Deref};
-use structopt::lazy_static::lazy_static;
+type ProcessorError =
+    blueprint_sdk::event_listeners::core::Error<blueprint_sdk::event_listeners::evm::error::Error>;
 
 sol!(
     #[allow(missing_docs)]
@@ -21,19 +25,19 @@ load_abi!(
     "contracts/out/TangleTaskManager.sol/TangleTaskManager.json"
 );
 
-lazy_static! {
-    pub static ref TASK_MANAGER_ADDRESS: Address = std::env::var("TASK_MANAGER_ADDRESS")
+pub static TASK_MANAGER_ADDRESS: LazyLock<Address> = LazyLock::new(|| {
+    std::env::var("TASK_MANAGER_ADDRESS")
         .map(|addr| addr.parse().expect("Invalid TASK_MANAGER_ADDRESS"))
-        .unwrap_or_else(|_| address!("0000000000000000000000000000000000000000"));
-}
+        .unwrap_or_else(|_| address!("0000000000000000000000000000000000000000"))
+});
 
 #[derive(Clone)]
 pub struct ExampleContext {
-    pub config: sdk::config::StdGadgetConfiguration,
+    pub config: GadgetConfiguration,
 }
 
 /// Returns "Hello, {who}!"
-#[sdk::job(
+#[job(
     id = 0,
     params(who),
     event_listener(
@@ -49,10 +53,10 @@ pub fn say_hello(context: ExampleContext, who: String) -> Result<String, Infalli
 
 /// Example pre-processor for handling inbound events
 async fn example_pre_processor(
-    (_event, log): (TangleTaskManager::NewTaskCreated, alloy_rpc_types::Log),
-) -> Result<(String,), gadget_sdk::Error> {
+    (_event, log): (TangleTaskManager::NewTaskCreated, Log),
+) -> Result<Option<(String,)>, ProcessorError> {
     let who = log.address();
-    Ok((who.to_string(),))
+    Ok(Some((who.to_string(),)))
 }
 
 #[cfg(test)]
@@ -61,7 +65,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let config = sdk::config::StdGadgetConfiguration::default();
+        let config = GadgetConfiguration::default();
         let context = ExampleContext { config };
         let result = say_hello(context, "Alice".into()).unwrap();
         assert_eq!(result, "Hello, Alice!");
